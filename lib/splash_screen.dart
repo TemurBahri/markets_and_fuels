43f261bfc526d5654be4250f71e97b46168cba12
TemurBahri <timurbahri@outlook.com>
2023-02-02 21:42:28 +0300

    Second Commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,24 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:lottie/lottie.dart';
+// ignore: depend_on_referenced_packages
+import 'package:page_transition/page_transition.dart';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Lottie.asset('assets/loading-circle.json'),
+      backgroundColor: const Color.fromARGB(255, 240, 85, 85),
+      nextScreen: const Scaffold(),
+      splashIconSize: 250,
+      duration: 3000,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.leftToRightWithFade,
+      animationDuration: const Duration(seconds: 1),
+    );
+  }
+}
